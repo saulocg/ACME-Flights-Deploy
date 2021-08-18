4c93cc0d39dddf4cc1678fd7491e91208c44f721
Fernando Dumont <fernando.dumont@appdynamics.com>
2021-08-18 19:32:58 -0300

    Improvements

--- config/web-frontend/env.js
+++ config/web-frontend/env.js
@@ -1,4 +1,5 @@
 (function (window) {
   window["env"] = window["env"] || {};
-  window["env"]["API_URL"] = "http://web-api:8080";
+  window["env"]["API_URL"] = "http://api-manager:8080";
+  window["env"]["WS_URL"] = "http://ws-services:8081";
 })(this);
