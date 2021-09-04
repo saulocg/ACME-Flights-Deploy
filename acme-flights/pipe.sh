7c0551ad96a141e6da19f9366309bf134bde7913
Fernando Dumont <fernando@fernando.com.br>
2021-09-04 15:09:24 -0300

    Update pipe.sh

--- acme-flights/pipe.sh
+++ acme-flights/pipe.sh
@@ -4,5 +4,7 @@
 # Criando pipe parar receber comandos do container e executar no host
 rm -rf "$PWD/pipe/acme"
 mkfifo "$PWD/pipe/acme"
+touch "$PWD/pipe/acme.log"
+chmod 777 "$PWD/pipe/acme.log"
 
 while true; do eval "$(cat $PWD/pipe/acme)" &> $PWD/pipe/acme.log ; done
