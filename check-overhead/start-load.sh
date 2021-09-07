ba1ca82b6a0021eca7797abce9fa92e4bbf8def7
Fernando Dumont <fernando@fernando.com.br>
2021-09-07 17:27:56 -0300

    Update start-load.sh

--- check-overhead/start-load.sh
+++ check-overhead/start-load.sh
@@ -2,4 +2,4 @@
 
 docker stop loader-basic loader-use-case
 
-docker run -itd --rm --name loader-api --network=host --env ENABLE_API_LOAD=1 -v $PWD/api-loader.yml:/app/config/api-loader.yml fernandodumont/acme-flights-loaders:latest
+docker run -it --rm --name loader-api --network=host --env ENABLE_API_LOAD=1 -v $PWD/api-loader.yml:/app/config/api-loader.yml fernandodumont/acme-flights-loaders:latest
