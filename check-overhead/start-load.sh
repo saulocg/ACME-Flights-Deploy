1166c5985a044cd2af67805d911326c5f776086f
Fernando Dumont <fernando@fernando.com.br>
2021-09-07 18:07:11 -0300

    Check Overhead

--- check-overhead/start-load.sh
+++ check-overhead/start-load.sh
@@ -2,7 +2,7 @@
 
 docker stop loader-basic loader-use-case
 
-ACME="http://localhost:8080"
+ACME="localhost:8080"
 JSON="{\"urlApi\":\"\"}"
 
 curl -s \
@@ -13,7 +13,7 @@ curl -s \
     --header "Cache-Control: no-cache" \
     --data "${JSON}" \
     -X POST \
-    $ACME/api/external-api
+    http://$ACME/api/external-api
 
 docker run -it --rm --name loader-api --network=host --env ENABLE_API_LOAD=1 -v $PWD/api-loader.yml:/app/config/api-loader.yml fernandodumont/acme-flights-loaders:latest
 
